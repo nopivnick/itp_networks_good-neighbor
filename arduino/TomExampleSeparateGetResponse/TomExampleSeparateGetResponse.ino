2901af4d8b6031893f08981d07f90074278a54bc
nopivnick <nopivnick@nopivnick.com>
2019-11-27 20:47:19 -0500

    Update TomExampleSeparateGetResponse.ino
    Working in this sketch:
    - GET request every five seconds
     - grabbing http response code
    - parsing JSON only if we received a valid http response code

--- arduino/TomExampleSeparateGetResponse/TomExampleSeparateGetResponse.ino
+++ arduino/TomExampleSeparateGetResponse/TomExampleSeparateGetResponse.ino
@@ -10,6 +10,7 @@
 */
 #include <ArduinoHttpClient.h>
 #include <WiFiNINA.h>
+#include <Arduino_JSON.h>
 
 #include "arduino_secrets.h"
 
@@ -18,8 +19,8 @@
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
-char serverAddress[] = "www.google.com";  // server address
-int port = 80;
+char serverAddress[] = "10.23.11.246";  // server address
+int port = 8080;
 
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, serverAddress, port);
@@ -27,6 +28,13 @@ int status = WL_IDLE_STATUS;
 
 long lastRequest = 0;
 long interval = 5000;
+
+int statusCode = -1;
+
+boolean haveStatusCode = false;
+
+// Declare variables to store values from the server
+int myPlayState;
 
 void setup() {
   Serial.begin(9600);
@@ -52,24 +60,46 @@ void loop() {
 
   if (millis() - lastRequest > interval) {
     Serial.println("making a request");
-    client.get("/");
+    // assemble the path for the GET message:
+    String pathState = "/playState";
+    client.get(pathState);
     lastRequest = millis();
+
+    // read the status code and body of the response
+    statusCode = client.responseStatusCode();
+    haveStatusCode = true;
+    Serial.print("Status code:\t");
+    Serial.println(statusCode);
   }
 
-  // read the status code and body of the response
-  int statusCode = client.responseStatusCode();
-  Serial.println("whee!");
-
-
-  if (statusCode >= 0) { // basically if the server sends any response code
+  if (statusCode >= 0 && haveStatusCode == true) { // basically if the server sends any valid http response code
     Serial.println("Send up the white smoke! We have a response!");
-      String response = client.responseBody();
-      Serial.print("Status code: ");
-      Serial.println(statusCode);
-      Serial.print("Response: ");
-      Serial.println(response);
+    String response = client.responseBody();
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    Serial.print("Response: ");
+    Serial.println(response);
+    // parse the string into a JSONVar object:
+    JSONVar myObject = JSON.parse(response);
+    // myObject.keys() can be used to get an array
+    // of all the keys in the object
+    JSONVar keys = myObject.keys();
+    Serial.println();
+    Serial.println("→ reading key values from the JSON response");
+    // read over the object, one key at a time:
+    for (int i = 0; i < keys.length(); i++) {
+      JSONVar value = myObject[keys[i]];
+      // what's the key:
+      Serial.print(keys[i]);
+      Serial.print("\t");
+      // and what's the value of the key:
+      Serial.print(value);
+      // and what's the type of the value:
+      Serial.print("\ttype: ");
+      Serial.println(JSON.typeof(value));
+    }
+    haveStatusCode = false;
   }
-
 
   //  Serial.println("Wait five seconds");
   //  delay(5000);
