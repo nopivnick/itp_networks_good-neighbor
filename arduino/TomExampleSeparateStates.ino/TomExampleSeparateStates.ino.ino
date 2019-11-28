f69b94287a40b3c38926225e98c1746bf35c31b0
nopivnick <nopivnick@nopivnick.com>
2019-11-28 12:13:45 -0500

    Update TomExampleSeparateStates.ino.ino
    Starting to flesh out the suedocode.

--- arduino/TomExampleSeparateStates.ino/TomExampleSeparateStates.ino.ino
+++ arduino/TomExampleSeparateStates.ino/TomExampleSeparateStates.ino.ino
@@ -1,16 +1,30 @@
+/*
+   Some in-progress code for toggling the device UI (e.g., volume level)
+   between my device state and my neighbor's device state.
+   
+   Example states for each device:
+   - just listening to buttons
+   - just making a request
+   - listening to buttons while I wait for a response
+*/
+
 long interval = 10000;  // 10 secs
 long lastServerCheck = 0;
 
+// We've run out of digital pins so we need to use an analog as digital
+// NOTE: when using analog as digigal it's recommended  to keep as much distance
+// from other anolog pins in use inorder to reduce noise / interference
+int buttonPeekPin = A6;
+boolean buttonPeekState = false;
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  if (toggleButtonChanged) {
-    makeServerCallForOtherNeighbor();
+  if (buttonPeekState = true) {
+    getNeighborDeviceState();
   }
 
   // periodic check of server:
@@ -22,13 +36,8 @@ void loop() {
   }
 }
 
-makeServerCallForOtherNeighbor();
+getNeighborDeviceState(); {
+}
 
-serverCheck();
-
-
-
-//states for our device
-//- just listening to buttons
-//- just making a request
-//- listening to buttons while I wait for a response
+serverCheck(); {
+}
