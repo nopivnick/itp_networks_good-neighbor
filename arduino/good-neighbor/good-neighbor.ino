fa7c4a5abe26ba726c00a1a1250607054ba8c36d
nopivnick <nopivnick@nopivnick.com>
2019-12-01 16:30:32 -0500

    Update good-neighbor.ino
    - replace with code from TomExampleSeparateGetResponse.ino

--- arduino/good-neighbor/good-neighbor.ino
+++ arduino/good-neighbor/good-neighbor.ino
@@ -5,6 +5,7 @@
     https://github.com/tigoe/Wifi101_examples/tree/master/HttpClientGetJSON
 
 */
+
 #include <ArduinoHttpClient.h>
 #include <WiFiNINA.h>
 #include <Arduino_JSON.h>
@@ -30,34 +31,43 @@ char pass[] = SECRET_PASS;
 #define BREAKOUT_DCS    7      // VS1053 Data/command select pin (output)
 #define CARDCS 6     // Card chip select pin
 #define DREQ A1       // VS1053 Data request, ideally an Interrupt pin
+#define REQUEST_STRING "\
+playState=myPlayState\
+\
+"
 
-// TODO: configure DNS for use with Glitch
-//IPAddress dns(8, 8, 8, 8);  // Google's primary DNS server
-
-//const char serverAddress[] = "itp-good-neighbor.glitch.me";  // server address @ glitch
-const char serverAddress[] = "10.23.11.246";                 // server address @ localhost on sandbox
-//const char serverAddress[] = "192.168.1.244";                // server address @ localhost on flame
+char serverAddress[] = "10.23.11.246";  // server address
 int port = 8080;
 
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, serverAddress, port);
 int status = WL_IDLE_STATUS;
 
+long lastRequest = 0;
+long interval = 5000;
+
+int statusCode = -1;
+
+boolean haveStatusCode = false;
+
+// Declare variables to store values from the server
+int myPlayState;
+//int myDecibels;
+//int myWarning;
+//int myVolume;
+//int myTrack;
+//int myDatetime;
+
 //instanciate a musicPlayer object
 Adafruit_VS1053_FilePlayer musicPlayer =
   Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
 
-// Declare variables to store values from the server
-int myPlayState;
-int myDecibels;
-int myWarning;
-int myVolume;
-int myTrack;
-int myDatetime;
-
 // Declare variables for the mp3 musicPlayer
-const int buttonPin = 1;      // pin for the pushbutton
-int lastButtonState = HIGH;   // previous state of the pushbutton
+const int buttonMinePin = 1;      // pin for my play/pause button
+int lastButtonMineState = HIGH;   // previous state of my play/pause button
+
+const int buttonGodPin = 14;      // pin for the 'God' play/pause button
+int lastButtonGodState = 0;       // previous state of the 'God' play/pause button
 
 const char soundFile[] = "track001.MP3";
 
@@ -65,13 +75,11 @@ const int ledPin1 =  13; // volume indicater LED pin - low
 const int ledPin2 =  12; // volume indicater LED pin -medium
 const int ledPin3 =  11; // volume indicater LED pin - high
 
-
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
   while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);     // print the network name (SSID);
+    Serial.println(ssid);                   // print the network name (SSID);
 
     // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
@@ -81,16 +89,10 @@ void setup() {
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your WiFi device's IP address:
+  // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
-
-  // TODO: configure DNS for use with Glitch
-  //  // print your DNS server address:
-  //  IPAddress dns = WiFi.setDNS(8.8.8.8);
-  //  Serial.print("Dns configured: ");
-  //  Serial.println(dns);
 
   /*
     mp3 stuff
@@ -99,7 +101,8 @@ void setup() {
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonMinePin, INPUT_PULLUP);
+  pinMode(buttonGodPin, INPUT_PULLUP);
   pinMode(BREAKOUT_RESET, OUTPUT);
   digitalWrite(BREAKOUT_RESET, LOW);
   delay(10);
@@ -127,11 +130,74 @@ void setup() {
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
   musicPlayer.startPlayingFile("/track001.mp3");
   Serial.println("playing");
-
 }
 
-
 void loop() {
+
+  // POST stuff
+
+  // assemble the path for the POST message:
+  String postData = String(REQUEST_STRING);
+  String path = "/playState";
+  String contentType = "application/x-www-form-urlencoded";
+
+  // GET stuff
+
+  if (millis() - lastRequest > interval) {
+    Serial.println("making a request");
+    // assemble the path for the GET message:
+    String pathState = "/playState";
+    client.get(pathState);
+    lastRequest = millis();
+
+    // read the status code and body of the response
+    statusCode = client.responseStatusCode();
+    haveStatusCode = true;
+    Serial.print("Status code:\t");
+    Serial.println(statusCode);
+  }
+
+  if (statusCode >= 0 && haveStatusCode == true) { // basically if the server sends any valid http response code
+    Serial.println("Send up the white smoke! We have a response!");
+    String response = client.responseBody();
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    Serial.print("Response: ");
+    Serial.println(response);
+    // parse the string into a JSONVar object:
+    JSONVar myObject = JSON.parse(response);
+    // myObject.keys() can be used to get an array
+    // of all the keys in the object
+    JSONVar keys = myObject.keys();
+    Serial.println();
+    Serial.println("→ reading key values from the JSON response");
+    // read over the object, one key at a time:
+    for (int i = 0; i < keys.length(); i++) {
+      JSONVar value = myObject[keys[i]];
+      // what's the key:
+      Serial.print(keys[i]);
+      Serial.print("\t");
+      // and what's the value of the key:
+      Serial.print(value);
+      // and what's the type of the value:
+      Serial.print("\ttype: ");
+      Serial.println(JSON.typeof(value));
+
+      // assign any string values got from the server to
+      // corresponding variables on client as integers
+
+      if (JSON.typeof(myObject["playState"]) == "string") {
+        // convert to int
+        String temp = JSON.stringify(myObject["playState"]);
+        //Serial.println(temp);
+        temp = temp.substring(1, -1);
+        myPlayState = temp.toInt();
+        //Serial.println("it's a string!");
+        //Serial.println(myPlayState);
+      }
+    }
+    haveStatusCode = false;
+  }
 
   /*
     mp3 player stuff
@@ -170,13 +236,31 @@ void loop() {
   }
 
   // read a pushbutton for play/pause:
-  int buttonState = digitalRead(buttonPin);
-  Serial.print("buttonState\t");
-  Serial.println(buttonState);
+  int buttonMineState = digitalRead(buttonMinePin);
   // if the button has changed:
-  if (buttonState != lastButtonState) {
+  if (buttonMineState != lastButtonMineState) {
     // if the button is low:
-    if (buttonState == LOW) {
+    if (buttonMineState == LOW) {
+      // switch play/pause state:
+      if (musicPlayer.paused()) {
+        musicPlayer.pausePlaying(false);
+        // send the POST request
+        client.post(path, contentType, postData);
+        Serial.println(postData);
+      } else {
+        musicPlayer.pausePlaying(true);
+      }
+    }
+  }
+  // save current button state for comparison next time:
+  lastButtonMineState = buttonMineState;
+
+  // read a pushbutton for play/pause:
+  int buttonGodState = myPlayState;
+  // if the button has changed:
+  if (buttonGodState != lastButtonGodState) {
+    // if the button is low:
+    if (buttonGodState == 1) {
       // switch play/pause state:
       if (musicPlayer.paused()) {
         musicPlayer.pausePlaying(false);
@@ -186,151 +270,7 @@ void loop() {
     }
   }
   // save current button state for comparison next time:
-  lastButtonState = buttonState;
-
-  /*
-     Server-client stuff
-  */
-
-  // assemble the path for the GET message:
-  String pathState = "/playState";
-  // String pathDecibels = "/decibels";
-  // String pathWarning = "/warning";
-  // String pathVolume = "/volume";
-  // String pathTrack = "/track";
-  // String pathDatetime = "/datetime";
-
-  // send the GET request
-  Serial.println("→ making GET request");
-  client.get(pathState);
-  // client.get(pathDecibels);
-  // client.get(pathWarning);
-  // client.get(pathVolume);
-  // client.get(pathTrack);
-  // client.get(pathDatetime);
-
-  // read the status code and body of the response
-  int statusCode = client.responseStatusCode();
-  String response = client.responseBody();
-  Serial.print("Status code: ");
-  Serial.println(statusCode);
-  Serial.print("Response: ");
-  Serial.println(response);
-
-  // parse the string into a JSONVar object:
-  JSONVar myObject = JSON.parse(response);
-
-  // if you know the object's keys, you can ask for their values:
-  //  Serial.print("playState:\t");
-  //  Serial.println(myObject["playState"]);
-  //  Serial.print("decibels:\t");
-  //  Serial.println(myObject["decibels"]);
-  //  Serial.print("warning:\t");
-  //  Serial.println(myObject["warning"]);
-  //  Serial.print("volume:\t\t");
-  //  Serial.println(myObject["volume"]);
-  //  Serial.print("track:\t\t");
-  //  Serial.println(myObject["track"]);
-  //  Serial.print("datetime:\t");
-  //  Serial.println(myObject["datetime"]);
-
-  // myObject.keys() can be used to get an array
-  // of all the keys in the object
-  JSONVar keys = myObject.keys();
-  //  Serial.println();
-  //  Serial.println("→ reading key values from the JSON response");
-  // read over the object, one key at a time:
-  for (int i = 0; i < keys.length(); i++) {
-    JSONVar value = myObject[keys[i]];
-    // what's the key:
-    //    Serial.print(keys[i]);
-    //    Serial.print("\t");
-    // and what's the value of the key:
-    //    Serial.print(value);
-    // and what's the type of the value:
-    //    Serial.print("\ttype: ");
-    //    Serial.println(JSON.typeof(value));
-  }
-
-  // assign values got from the server to corresponding variables on client
-  //  Serial.println();
-  //  Serial.println("→ assigning server values to variables on client");
-
-  if (JSON.typeof(myObject["playState"]) == "string") {
-    // convert to int
-    String temp = JSON.stringify(myObject["playState"]);
-    //    Serial.println(temp);
-    temp = temp.substring(1, -1);
-    myPlayState = temp.toInt();
-    //    Serial.println("it's a string!");
-  }
-  if (JSON.typeof(myObject["playState"]) == "number") {
-    // it's already an int
-    myPlayState = myObject["playState"];
-    //    Serial.println("it's a number!");
-  }
-  //  Serial.print("myPlayState:\t");
-  //  Serial.println(myPlayState);
-
-  if (JSON.typeof(myObject["decibels"]) == "string") {
-    // convert to int
-    String temp = JSON.stringify(myObject["decibels"]);
-    temp = temp.substring(1, -1);
-    myDecibels = temp.toInt();
-  }
-  if (JSON.typeof(myObject["decibels"]) == "number") {
-    // it's already an int
-    myDecibels = myObject["decibels"];
-  }
-  //  Serial.print("myDecibels:\t");
-  //  Serial.println(myDecibels);
-
-  if (JSON.typeof(myObject["warning"]) == "string") {
-    // convert to int
-    String temp = JSON.stringify(myObject["warning"]);
-    temp = temp.substring(1, -1);
-    myWarning = temp.toInt();
-  }
-  if (JSON.typeof(myObject["warning"]) == "number") {
-    // it's already an int
-    myWarning = myObject["warning"];
-  }
-  //  Serial.print("myWarning:\t");
-  //  Serial.println(myWarning);
-
-  if (JSON.typeof(myObject["volume"]) == "string") {
-    // convert to int
-    String temp = JSON.stringify(myObject["volume"]);
-    temp = temp.substring(1, -1);
-    myVolume = temp.toInt();
-  }
-  if (JSON.typeof(myObject["volume"]) == "number") {
-    // it's already an int
-    myVolume = myObject["volume"];
-  }
-  //  Serial.print("myVolume:\t");
-  //  Serial.println(myVolume);
-
-  if (JSON.typeof(myObject["track"]) == "string") {
-    // convert to int
-    String temp = JSON.stringify(myObject["track"]);
-    temp = temp.substring(1, -1);
-    myTrack = temp.toInt();
-  }
-  if (JSON.typeof(myObject["track"]) == "number") {
-    // it's already an int
-    myTrack = myObject["track"];
-  }
-  //  Serial.print("myTrack:\t");
-  //  Serial.println(myTrack);
-
-  myDatetime = myObject["datetime"];
-  //  Serial.print("myDatetime:\t");
-  //  Serial.println(myDatetime);
-
-  Serial.println("Wait one second\n");
-  delay(1000);
-
+  lastButtonGodState = buttonGodState;
 }
 
 void printDirectory(File dir, int numTabs) {
