306b15f1316ca7b4af93ae720649703ef52e6bab
nopivnick <nopivnick@nopivnick.com>
2019-12-03 23:04:16 -0500

    Update web client
    - Add button to send POST request
    - Add comic depicting neighbors
    - Add Google Web Font (Roboto)
    - Add css

--- public/admin/sketch.js
+++ public/admin/sketch.js
@@ -1,35 +1,54 @@
+let myVolume = 0;
+let neighbor;
+
+let button;
+let img;
+
+function preload() {
+  // Get current json from the server API
+  let url = 'http://10.23.11.246:8080/volume';
+
+  neighbor = loadJSON(url);
+  img = loadImage('assets/comic.png')
+}
+
 function setup() {
+  createCanvas(1920, 1080);
+  image(img, 10, 10, 1290, 675);
+  button = createButton('STOP MUSIC');
+  let col = color(255, 20, 30);
+  let white = color(255);
+
+  button.style('background-color', col);
+  button.style('color', white);
+  button.style('borded', 0);
+  button.style('font-size', '20px');
+  button.style('font-family', 'roboto');
+  button.position(970, 30);
+  button.size(300, 230);
+  button.mousePressed(makePost);
   //  noCanvas();
 }
 
-function mousePressed() {
-  // let myPlayState = {
-  //   playState: 1
-  // };
-
-  console.log("mouse clicked");
+function makePost() {
+  console.log("buttom clicked");
   let url = 'http://10.23.11.246:8080/playState/1';
   //let content = JSON.stringify(myPlayState);
   // httpPost(url, 'json', myPlayState, function(response) {
   //   print(response);
   // });
 
-  // $.post("http://10.23.11.246:8080/playState/1", null, function(data, status) {
-  //   console.log(`${data} and status ${status}`)
-  // });
   httpDo(url, {
       method: 'POST',
     },
     function(res) {
-      print("success");
+      print(res);
     }
   );
   // print(myPlayState);
 
-
-
 }
 
 function draw() {
-  background(220);
+
 }
